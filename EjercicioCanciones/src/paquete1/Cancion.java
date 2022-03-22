d7a2164d5228eb4b779b13d83194adc76e8a92d4
leo <leo@gmail.com>
2022-03-22 15:56:20 +0100

    Modificacion del merodo toString

--- EjercicioCanciones/src/paquete1/Cancion.java
+++ EjercicioCanciones/src/paquete1/Cancion.java
@@ -119,7 +119,11 @@ public class Cancion {
 
 	@Override
 	public String toString() {
-		return "Cancion [titulo=" + titulo + ", autor=" + grupo + ", duracion=" + duracion + ", sonando=" + sonando
-				+ "]";
+	return "Datos de la canción [titulo=" + titulo + ", autor=" +
+
+	grupo + ", duracion=" + duracion + ", sonando=" + sonando
+
+	+ "]";
+
 	}
 }
